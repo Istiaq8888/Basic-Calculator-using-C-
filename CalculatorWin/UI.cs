d64d8659c1049f92c7e64795b1cecd43d1fad26f
DESKTOP-N76GGL6\Md.Istiaq Ahmed <istiaq88888@gmail.com>
2023-08-02 10:24:57 +0800

    Basic calculator first commit

---
+++ CalculatorWin/UI.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibraryCalculations;
+
+namespace CalculatorWin
+{
+    public partial class UI : Form
+    {
+        private Calculator calculator;
+
+        private bool isOperationClicked = false;
+        private bool isDecimalClicked = false;
+
+        public UI()
+        {
+            InitializeComponent();
+            calculator = new Calculator();
+            isOperationClicked = false;
+            isDecimalClicked = false;
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+
+            Button button = (Button)sender;
+            if (textBox_Result.Text == "0" || isOperationClicked)
+            {
+                textBox_Result.Text = button.Text;
+                isOperationClicked = false;
+            }
+            else
+            {
+                textBox_Result.Text += button.Text;
+            }
+
+        }
+
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            try
+            {
+                double num = Convert.ToDouble(textBox_Result.Text);
+                calculator.PerformOperation(num);
+            }
+            catch (Exception ex)
+            {
+                textBox_Result.Text = "Error";
+                labelCurrentOperation.Text = "";
+            }
+            calculator.SetCurrentOperation(button.Text);
+            labelCurrentOperation.Text = textBox_Result.Text + " " + button.Text;
+            isOperationClicked = true;
+            isDecimalClicked = false;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double num = Convert.ToDouble(textBox_Result.Text);
+                double result = calculator.PerformOperation(num);
+                textBox_Result.Text = result.ToString();
+                labelCurrentOperation.Text = "";
+            }
+            catch (Exception ex)
+            {
+                textBox_Result.Text = "Error";
+                labelCurrentOperation.Text = "";
+            }
+            isDecimalClicked = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+            isDecimalClicked = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            calculator.Clear();
+            textBox_Result.Text = "0";
+            labelCurrentOperation.Text = "";
+            isOperationClicked = false;
+            isDecimalClicked = false;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            if (!isDecimalClicked)
+            {
+                textBox_Result.Text += ".";
+                isDecimalClicked = true;
+            }
+        }
+    }
+}
---
+++ ClassLibraryCalculations/Engine.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCalculations
+{
+    public class Calculator
+    {
+        private double result;
+        private string currentOperation;
+
+        private bool isDecimalClicked;
+
+        public Calculator()
+        {
+            result = 0;
+            currentOperation = "";
+            isDecimalClicked = false;
+        }
+
+        public void SetCurrentOperation(string operation)
+        {
+            currentOperation = operation;
+            isDecimalClicked = false;
+        }
+
+        public double PerformOperation(double number)
+        {
+            switch (currentOperation)
+            {
+                case "+":
+                    result += number;
+                    break;
+                case "-":
+                    result -= number;
+                    break;
+                case "X":
+                    result *= number;
+                    break;
+                case "/":
+                    if (number != 0)
+                    {
+                        result /= number;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException("Cannot divide by zero.");
+                    }
+                    break;
+                case "MOD":
+                    result %= number;
+                    break;
+                default:
+                    result = number;
+                    break;
+            }
+            return result;
+        }
+
+        public void Clear()
+        {
+            result = 0;
+            currentOperation = "";
+            isDecimalClicked = false;
+        }
+
+        public bool IsValidInput(string input)
+        {
+            // Check if the input contains more than one decimal point
+            int decimalPointCount = 0;
+            foreach (char c in input)
+            {
+                if (c == '.')
+                {
+                    decimalPointCount++;
+                    if (decimalPointCount > 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
